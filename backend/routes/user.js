ed0c86693bd18dbebd6a70f69b2dc701b5674be2
sourabhcse <sourabhsaini201098@gmail.com>
2023-01-23 16:42:55 +0530

    login

---
+++ backend/routes/user.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const userController = require("../controllers/users");
+const router = express.Router();
+
+router.post("/user/signup", userController.singUpUser);
+router.post("/user/login", userController.loginUser);
